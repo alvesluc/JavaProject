2fa22f0292f95a91d6928dbd9fed56b0a2bae7d2
Lucas Alves <42820357+alvesluc@users.noreply.github.com>
2019-06-09 15:41:28 -0300

    Alteração no funcionamento dos fornecedores

--- src/fornecedores/Fornecedor.java
+++ src/fornecedores/Fornecedor.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 import caixa.ArquivoSerializadoUtil;
 import caixa.Caixa;
 import caixa.Operacoes;
+import informacoes.ArquivoSerializadoUtilInformacoes;
 import informacoes.ListaCategoria;
 import informacoes.ListaProd;
 import informacoes.Produto;
@@ -27,11 +28,11 @@ public class Fornecedor {
 			if (file.mkdir()) {
 				System.out.println("Fornecedor adcionado!");
 			} else {
-				System.out.println("Falha na adi��o do fornecedor!");
+				System.out.println("Falha na adição do fornecedor!");
 			}
 		}
 	}
-	
+
 	public static void alteraFornecedor(String fornecedor) {
 
 		File dir = new File("FORNECEDORES/" + fornecedor + "/");
@@ -39,7 +40,7 @@ public class Fornecedor {
 		Scanner leitor = new Scanner(System.in);
 
 		if (!dir.isDirectory()) {
-			System.err.println("Esse fornecedor n�o existe.");
+			System.err.println("Esse fornecedor não existe.");
 		} else {
 			System.out.println("Informe o novo nome do fornecedor: ");
 			String novoNome = leitor.nextLine();
@@ -47,111 +48,120 @@ public class Fornecedor {
 			dir.renameTo(newDir);
 		}
 	}
-	
+
 	public static void removeFornecedor(String fornecedor) {
-        String folder = "CATEGORIA/"+ fornecedor + "/";
-        //delete folder recursively
-        recursiveDelete(new File(folder));
-    }
-    
-    public static void recursiveDelete(File file) {
-        //to end the recursive loop
-        if (!file.exists())
-            return;
-        
-        //if directory, go inside and call recursively
-        if (file.isDirectory()) {
-            for (File f : file.listFiles()) {
-                //call recursively
-                recursiveDelete(f);
-            }
-        }
-        //call delete to delete files and empty directory
-        file.delete();
-        System.out.println("Fornecedor apagado: "+file.getAbsolutePath());
-    }
-    
-    public static void listaFornecedores() {
-		
+		String folder = "CATEGORIA/" + fornecedor + "/";
+		// delete folder recursively
+		recursiveDelete(new File(folder));
+	}
+
+	public static void recursiveDelete(File file) {
+		// to end the recursive loop
+		if (!file.exists())
+			return;
+
+		// if directory, go inside and call recursively
+		if (file.isDirectory()) {
+			for (File f : file.listFiles()) {
+				// call recursively
+				recursiveDelete(f);
+			}
+		}
+		// call delete to delete files and empty directory
+		file.delete();
+		System.out.println("Fornecedor apagado: " + file.getAbsolutePath());
+	}
+
+	public static void listaFornecedores() {
+
 		File file = new File("FORNECEDORES/");
 		String[] directories = file.list(new FilenameFilter() {
-		  public boolean accept(File current, String name) {
-		    return new File(current, name).isDirectory();
-		  }
+			public boolean accept(File current, String name) {
+				return new File(current, name).isDirectory();
+			}
 		});
-		
+
 		System.out.println(Arrays.toString(directories));
-		
+
 	}
-    
-    public static void compraFornecedor() {
-    	
-    	Caixa caixaLoja = (Caixa)ArquivoSerializadoUtil.lerCaixaSerializado();
+
+	public static void compraFornecedor() {
+
+		Caixa caixaLoja = (Caixa) ArquivoSerializadoUtil.lerCaixaSerializado();
 		float disponivel = caixaLoja.getDinheirocaixa();
-    	System.out.println("Valor dispon�vel para compra: " + caixaLoja.getDinheirocaixa());
-		
-    	System.out.println("Escolha um fornecedor e sua categoria: ");
-    	listaFornecedores();
-    	ListaCategoria.listaCategoria();
-    	@SuppressWarnings("resource")
+		System.out.println("Valor disponível para compra: " + caixaLoja.getDinheirocaixa());
+
+		System.out.println("Escolha um fornecedor e sua categoria: ");
+		listaFornecedores();
+		ListaCategoria.listaCategoria();
+		@SuppressWarnings("resource")
 		Scanner leitor = new Scanner(System.in);
-    	
-    	System.out.println("Fornecedor: ");
-    	String fornecedor = leitor.nextLine();
-    	
-    	System.out.println("Categoria: ");
-    	String categoria = leitor.nextLine();
-    	
-    	File directory = new File("FORNECEDORES/" + fornecedor + "/CATEGORIA/" + categoria + "/");
-    	int fileCount = (int)directory.list().length;
-    	
-    	System.out.println(fileCount);
-		
-		for(int i = 1; i <= fileCount; i++) {
+
+		System.out.println("Fornecedor: ");
+		String fornecedor = leitor.nextLine();
+
+		System.out.println("Categoria: ");
+		String categoria = leitor.nextLine();
+
+		File directory = new File("FORNECEDORES/" + fornecedor + "/CATEGORIA/" + categoria + "/");
+		int fileCount = (int) directory.list().length;
+
+		for (int i = 1; i <= fileCount; i++) {
 			String id = Integer.toString(i);
-			ListaProd.fornecer(fornecedor, id);
+			ListaProd.fornecer(fornecedor, categoria, id);
 		}
-    	
+
 		String buscaID;
-		
+
 		float totalPreco = 0;
-		
+
 		System.out.println("Digite o ID do produto que deseja ou 0 para sair:");
 		buscaID = leitor.nextLine();
-	
-		while(!buscaID.equals("0")) {
-			
-			File existe = new File("FORNECEDORES/" + fornecedor + "/" + buscaID + ".txt");
-			boolean taOk = existe.exists();  
-			
-			if(taOk) {
-			
-				System.out.println("Digite a quantidade que deseja comprar: ");
-				int buscaQuant = Integer.parseInt(leitor.nextLine());	
-				
-				Produto pegarItem = (Produto)ArquivoSerializadoUtilFornecedor.lerArquivoSerializado(fornecedor, buscaID);
-				Produto pegarAlterado = pegarItem;
-				
-				totalPreco = pegarItem.getPrecovenda() * buscaQuant;
-				
-				
-				pegarItem.setQtde_estoque(pegarItem.getQtde_estoque() + buscaQuant);
-				
-				boolean gravou = ArquivoSerializadoUtilFornecedor.gravaObjetoSerializado(fornecedor, pegarItem.getId(), pegarAlterado);
-				
-				if(gravou) {
-					System.out.println("Tudo massa");
-				}
-				
-				}else {
-				System.out.println("Entre com um valor existente.");
-			}
-			System.out.println("Digite o ID do produto que deseja ou 0 para sair:");
-			buscaID = leitor.nextLine();
 
+		while (!buscaID.equals("0")) {
+
+			if (disponivel > 0) {
+				File existe = new File(
+						"FORNECEDORES/" + fornecedor + "/CATEGORIA/" + categoria + "/" + buscaID + ".txt");
+				boolean taOk = existe.exists();
+
+				if (taOk) {
+
+					System.out.println("Digite a quantidade que deseja comprar: ");
+					int buscaQuant = Integer.parseInt(leitor.nextLine());
+
+					Produto pegarItem = (Produto) ArquivoSerializadoUtilFornecedor.lerArquivoSerializado(fornecedor, categoria,
+							buscaID);
+					Produto pegarAlterado = pegarItem;
+
+					totalPreco = pegarItem.getPrecovenda() * buscaQuant;
+
+					if (totalPreco > disponivel) {
+						System.out.println("Você não tem dinheiro suficiente.");
+					} else {
+						pegarItem.setQtde_estoque(pegarItem.getQtde_estoque() + buscaQuant);
+						
+						boolean gravou = ArquivoSerializadoUtilInformacoes.gravaObjetoSerializado(categoria,
+								pegarItem.getId(), pegarAlterado);
+
+						if (gravou) {
+							disponivel = disponivel - totalPreco;
+							Operacoes.comprarComCaixa(disponivel);
+
+							System.out.println("Operação realizada com sucesso.");
+						}
+
+					}
+
+				} else {
+					System.out.println("Entre com um valor existente.");
+				}
+				System.out.println("Digite o ID do produto que deseja ou 0 para sair:");
+				buscaID = leitor.nextLine();
+			}
+			
 		}
-		disponivel = disponivel - totalPreco;
-		Operacoes.comprarComCaixa(disponivel);
-    }
+
+	}
 
 }
